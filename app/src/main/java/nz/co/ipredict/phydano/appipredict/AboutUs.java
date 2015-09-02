540ad0061b79e7cc8bf7a11462233eac70dfd186
phydano <ph.dano@gmail.com>
2015-09-03 10:08:40 +1200

    Have fixed the ArrayOutOfBound Exception with some checks on the array size. Currently work on the about us section. Clean up code.
    Plan to do the timeout for connection, work on the browse prediction and about us section. Need to get scroll view working.

--- app/src/main/java/nz/co/ipredict/phydano/appipredict/AboutUs.java
+++ app/src/main/java/nz/co/ipredict/phydano/appipredict/AboutUs.java
@@ -23,7 +23,7 @@ public class AboutUs extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_about_us, menu);
         return super.onCreateOptionsMenu(menu);
-        
+
     }
 
     @Override
